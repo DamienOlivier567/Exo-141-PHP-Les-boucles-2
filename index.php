3658d0c5e7fdb583354b55b4fc2b9abec4472d8f
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-01-22 15:08:38 +0100

    Exo-141 PHP

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$var = 0;
+while($var < 10){
+    echo $var . "<br>";
+    $var ++;
+}
+
+$var2 = 0;
+$var3 = 2;
+
+while($var2 < 20){
+    $var2 *= $var3;
+    echo $var2 . "<br>";
+    $var2++;
+}
+
+$var4 = 1;
+
+while($var4 < 10){
+    $var5 = $var4 * 0.5;
+    echo $var5 . "<br>";
+    $var4++;
+}
+
+for($i = 0; $i < 15; $i++){
+    echo "On arrive <br>";
+}
+
+for($i = 20; $i > 0; $i--){
+    echo "C'est presque bon <br>";
+}
+
+for($i = 0; $i < 100; $i+=15){
+    echo "On tient le bout <br>";
+}
+
+for($i = 200; $i > 0; $i-=12){
+    echo "Enfin !!!!! <br>";
+}
